647ea23440bcb8f0b21e45158003eeb8f93ecd7d
David Nitzsche-Bell <david@zicount.com>
2022-11-10 13:37:51 +0100

    Initial Commit

---
+++ SVRGE Relax/SVRGE_RelaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVRGE_RelaxApp.swift
+//  SVRGE Relax
+//
+//  Created by Zicount on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct SVRGE_RelaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
